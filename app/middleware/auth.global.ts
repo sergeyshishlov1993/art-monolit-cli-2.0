b2433901da294c09e9bd9d5902e649155e7f7ea4
sergey shishlov <shishlovsergey040593@gmail.com>
2026-03-02 21:37:55 +0200

    fix auth

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -3,6 +3,8 @@ import { useAuthStore } from '~/modules/auth/AuthStore'
 export default defineNuxtRouteMiddleware((to) => {
     if (!to.path.startsWith('/admin') || to.path === '/admin/login') return
 
+    if (import.meta.server) return
+
     const authStore = useAuthStore()
     authStore.init()
 
